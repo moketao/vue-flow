5be01ba1596ed31f90ecc0b7296dbf6634c6eef6
moketao <moketao@qq.com>
2021-12-14 16:56:29 +0800

    move naive-ui to peerDependencies

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      external: ['vue','naive-ui'],
       // plugins:[
       //   resolve()
       // ],
